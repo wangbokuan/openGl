b8becefae72a218380169f7b4736f78bb9b96a96
buwang <buwang@vw-mobvoi.com>
2020-03-17 14:41:53 +0800

    Init project

---
+++ app/src/main/java/com/voice/wb/opengl/MainActivity.java
@@ -0,0 +1,34 @@
+package com.voice.wb.opengl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * @Author WangBu
+ * @DATE 20-3-17 14:39
+ * E-Mail Address：android_wb@163.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
