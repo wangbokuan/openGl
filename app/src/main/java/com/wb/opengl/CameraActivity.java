1020b7993463e6e4edf09a97c1f889273aca4a6b
buwang <buwang@vw-mobvoi.com>
2020-05-25 17:18:04 +0800

    success to preview cameraC

--- app/src/main/java/com/wb/opengl/CameraActivity.java
+++ app/src/main/java/com/wb/opengl/CameraActivity.java
@@ -1,29 +1,45 @@
 package com.wb.opengl;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.ImageReader;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Size;
 import android.view.OrientationEventListener;
 import android.view.Surface;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
@@ -32,29 +48,212 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     //initView
     private TextView tv_pic;
     private TextView tv_video;
-    private SurfaceView mPrevoewSurface;
+    private TextureView mPrevoewSurface;
+
+    private CameraManager mCameraManager;
+    private CameraDevice mCameraDevice;
+    private ImageReader mImageReader;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+    private CameraCharacteristics mCameraCharacteristics;
+    private int mCameraId = CameraCharacteristics.LENS_FACING_FRONT;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private Surface mCameraSurface;
+    private CameraCaptureSession mCameraCaptureSession;
+    private CaptureRequest mCaptureRequest;
 
     //申请两个权限，录音和文件读写
     //1、首先声明一个数组permissions，将需要的权限都放在里面
-    String[] permissions = new String[]{Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA};
+    String[] permissions = new String[]{Manifest.permission.CAMERA};
     //2、创建一个mPermissionList，逐个判断哪些权限未授予，未授予的权限存储到mPerrrmissionList中
     List<String> mPermissionList = new ArrayList<>();
 
     private final int mRequestCode = 100;//权限请求码
+    private CameraDevice.StateCallback mStateCallBack = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCameraDevice = camera;
+            try {
+                mCameraDevice.createCaptureSession(Collections.singletonList(mCameraSurface), mSessionStateCallback, mBackgroundHandler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            releaseCamera();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            releaseCamera();
+        }
+    };
+
+    private CameraManager.AvailabilityCallback mAvailabilityCallback = new CameraManager.AvailabilityCallback() {
+        @Override
+        public void onCameraAvailable(@NonNull String cameraId) {
+            super.onCameraAvailable(cameraId);
+        }
+
+        @Override
+        public void onCameraUnavailable(@NonNull String cameraId) {
+            super.onCameraUnavailable(cameraId);
+        }
+    };
+
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            checkAuthor();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            mCameraCaptureSession = session;
+            requestPreview();
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+        }
+    };
+
+    private CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+            super.onCaptureStarted(session, request, timestamp, frameNumber);
+        }
+
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+        }
+
+        @Override
+        public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        checkAuthor();
+        startBackThread();
         tv_pic = findViewById(R.id.tv_picture);
         tv_video = findViewById(R.id.tv_video);
+        tv_pic.setOnClickListener(this);
+        tv_video.setOnClickListener(this);
         mPrevoewSurface = findViewById(R.id.surface_camera);
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerAvailabilityCallback(mAvailabilityCallback, mBackgroundHandler);
+        mPrevoewSurface.setSurfaceTextureListener(mSurfaceTextureListener);
     }
 
+
+    @SuppressLint("MissingPermission")
+    private void openCamera() {
+        try {
+            //获取摄像机参数
+            mCameraCharacteristics = mCameraManager.getCameraCharacteristics(Integer.toString(mCameraId));
+            //获取相机支持的预览尺寸列表
+            StreamConfigurationMap map = mCameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            // the support size of JPEG
+            Size[] outputSizes = map.getOutputSizes(ImageFormat.JPEG);
+            //select the largest size
+            Size largest = Collections.max(Arrays.asList(outputSizes), new CompareSizeByArea());
+            Log.d(TAG, "picture size:" + largest.getHeight() + " * " + largest.getWidth());
+            mPrevoewSurface.getSurfaceTexture().setDefaultBufferSize(largest.getWidth(), largest.getHeight());
+            mCameraSurface = new Surface(mPrevoewSurface.getSurfaceTexture());
+            mCameraManager.openCamera(String.valueOf(mCameraId), mStateCallBack, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void startBackThread() {
+        mBackgroundThread = new HandlerThread("CameraManagerBack");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void stopBackThread() {
+        try {
+            mBackgroundThread.quitSafely();
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void release() {
+        mCameraManager.unregisterAvailabilityCallback(mAvailabilityCallback);
+    }
+
+    private void releaseCamera() {
+        if (null != mCameraDevice) {
+            mCameraDevice.close();
+        }
+        stopBackThread();
+    }
+
+    private void requestPreview() {
+        try {
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(mCameraSurface);
+            mCaptureRequest = mCaptureRequestBuilder.build();
+            mCameraCaptureSession.setRepeatingRequest(mCaptureRequest, mCaptureCallback, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class CompareSizeByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            return Long.signum((long) (lhs.getWidth() * lhs.getHeight()) - (long) rhs.getWidth() * rhs.getHeight());
+        }
+    }
+
+
     private void checkAuthor() {
-        for (String permission:permissions){
+        for (String permission : permissions) {
             int checkPermission = checkSelfPermission(permission);
             if (checkPermission != PackageManager.PERMISSION_GRANTED || !PermisssionUtils.checkOpsPermission(this, permission)) {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
@@ -81,10 +280,10 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
             }
         }
 
-        if(mPermissionList.size()>0){
-                ActivityCompat.requestPermissions(CameraActivity.this, permissions, mRequestCode);
-        }else {
-            initCamera();
+        if (mPermissionList.size() > 0) {
+            ActivityCompat.requestPermissions(CameraActivity.this, permissions, mRequestCode);
+        } else {
+            openCamera();
         }
     }
 
@@ -93,7 +292,7 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         switch (requestCode) {
             case mRequestCode:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    initCamera();
+                    openCamera();
                 } else {
                     Toast.makeText(CameraActivity.this, "权限被拒绝", Toast.LENGTH_LONG).show();
                 }
@@ -118,13 +317,25 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void recordVideo() {
+        try {
+            mCameraCaptureSession.stopRepeating();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     private void takePicture() {
+        try {
+            mCameraCaptureSession.setRepeatingRequest(mCaptureRequest, mCaptureCallback, mBackgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initCamera() {
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseCamera();
+        release();
     }
-
 }
